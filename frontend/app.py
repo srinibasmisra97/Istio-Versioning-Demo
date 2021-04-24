2ba3fb45cbbce433869ab301de0e9b8e60f07a68
Srinibas Misra <srinibas.misra97@gmail.com>
2021-04-24 14:36:39 +0530

    updated frontend app

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,13 @@
 import requests, os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route("/")
 def index():
-    urls = {"url": os.environ.get("BACKEND_URL")}
-    return render_template("index.html", data=urls)
+    version = "live" if request.headers.get("version") is None else request.headers.get("version")
+    data = requests.get(os.environ.get("BACKEND_URL") + "/generate", headers={"version": version})
+    return render_template("index.html", data=data.text)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0')
